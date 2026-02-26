7799ff456cbff3b40810e7d060b940c8750daf89
gilbertketer <gilbertketer759@gmail.com>
2026-02-26 17:25:46 +0300

    DB Conn and Server Initialization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gilbert-keter/stellara/configs"
+)
+
+func main() {
+	db := configs.InitDB()
+	defer db.Close()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Server is running")
+	})
+
+	port := ":8080"
+	fmt.Printf("Server running on %s\n", port)
+	if err := http.ListenAndServe(port, mux); err != nil {
+		log.Fatalf("Server error: %v", err)
+	}
 }
